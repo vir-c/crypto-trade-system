1d4a0231838fa5837d25c72b89e9a7dd0c9a1354
virc <icbrvdr@gmail.com>
2021-05-03 17:54:56 +0530

    uncomment code

--- packages/aggregator-service/src/main.ts
+++ packages/aggregator-service/src/main.ts
@@ -12,18 +12,17 @@ export async function main() {
     try {
         // get price list from exchange
         const priceListUSDT = await exchangeAdaptor.getTickers()
-        console.log(priceListUSDT.map((item)=>item.avgPrice5min? item.avgPrice5min : 'LOL'))
 
-        // //add results to db
-        // await db.controller.ticker.addTicker({
-        //     exchange: 'binance',
-        //     priceList: priceListUSDT,
-        // })
+        //add results to db
+        await db.controller.ticker.addTicker({
+            exchange: 'binance',
+            priceList: priceListUSDT,
+        })
 
-        // console.log('Ticker added successful for ', new Date().toUTCString())
+        console.log('Ticker added successful for ', new Date().toUTCString())
 
-        // //message decider service
-        // ipc.relayToDeciderService()
+        //message decider service
+        ipc.relayToDeciderService()
     } catch (error) {
         const dateTime = new Date().toString()
         Log.error(error, `Failed to execute main at ${dateTime}... `)
